67f1c81c2ad6c8d9ec21ff1b795605b1b018d705
gml <majorgml@gmail.com>
2012-08-06 16:03:55 +0800

    moves with mouse movement

--- fps.py
+++ fps.py
@@ -6,6 +6,9 @@
 '''
 
 import pyglet
+from pyglet import clock
+
+
 
 window = pyglet.window.Window()
 window.set_caption('FPS ver D-0.0')
@@ -22,12 +25,60 @@ print bg.width
 print bg.width-(bg.width/3)
 ai = bg.get_region(bg.width/3, 0, bg.width-(bg.width/3*2), bg.height)
 
+movestop=100
+moveleft=110
+moveright=120
+direct=movestop
+leftside=bg.width/3
+rightside=bg.width/3*2
+
+def moveleft():
+    global leftside,rightside,ai
+    leftside-=1
+    rightside-=1
+    ai = bg.get_region(leftside, 0, rightside, bg.height)
+    pass
+
+def moveright():
+    global leftside,rightside,ai
+    leftside+=1
+    rightside+=1
+    ai = bg.get_region(leftside, 0, rightside, bg.height)
+    pass
+
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    #print x, y, dx, dy
+    w=window.width
+    h=window.height
+    if x>w/3*2:
+        #print 'right'
+        moveleft()
+    elif x < w/3:
+        #print 'left'
+        moveright()
+    else:
+        pass #print 'stopped'
+    
+    pass
+
+
 @window.event
 def on_draw():
     window.clear()
     #backgroundspr.blit(0,0,width=window.width,height=window.height)
     ai.blit(0,0,width=window.width,height=window.height)
     
-    
+def callback(dt):
+    pass
+
+clock.schedule_interval(callback, 1.0)
+clock.set_fps_limit(60)
+
 if __name__ == "__main__":
     pyglet.app.run()   
+
+
+
+
